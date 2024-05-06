eb36429b2aa21e3bd3ab941f25eb18bcdf0a9b05
Rubin <filiperubin@gmail.com>
2024-05-05 21:25:01 -0300

    Test input handling in game

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -11,6 +11,14 @@ int main()
 		gameWindow->GetRenderer()->SetBackgroundColor(0.5f, 0.3f, 0.1f, 1.0f);
 		while (gameWindow->ShouldContinueRunning())
 		{
+			if (gameWindow->GetInput()->IsKeyboardKeyJustPressed(KeyboardKey::A))
+			{
+				gameWindow->GetRenderer()->SetBackgroundColor(0.1f, 0.8f, 0.4f, 1.0f);
+			}
+			else if (gameWindow->GetInput()->IsKeyboardKeyJustPressed(KeyboardKey::D))
+			{
+				gameWindow->GetRenderer()->SetBackgroundColor(0.4f, 0.5f, 1.0f, 1.0f);
+			}
 			gameWindow->GetRenderer()->ClearScreen();
 			gameWindow->Process();
 		}
