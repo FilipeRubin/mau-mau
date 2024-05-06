53beaff6c7b7fa8920589a4f13864a335c5a44c5
Rubin <filiperubin@gmail.com>
2024-05-05 21:08:31 -0300

    Implemented class for input handling

---
+++ engine/src/input-implementation.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "engine/i-input.h"
+#include "i-input-core.h"
+
+class InputImplementation : public IInput, public IInputCore
+{
+public:
+	InputImplementation();
+	bool IsKeyboardKeyJustPressed(KeyboardKey key) const override;
+	bool IsKeyboardKeyJustReleased(KeyboardKey key) const override;
+	bool IsKeyboardKeyPressed(KeyboardKey key) const override;
+	bool IsMouseButtonJustPressed(MouseButton button) const override;
+	bool IsMouseButtonJustReleased(MouseButton button) const override;
+	bool IsMouseButtonPressed(MouseButton button) const override;
+	void Process() override;
+	void UpdateKeyboardKeyState(KeyboardKey key, bool isPressed) override;
+	void UpdateMouseButtonState(MouseButton button, bool isPressed) override;
+private:
+	bool m_keyboardKeysPressed[static_cast<int>(KeyboardKey::LAST) + 1];
+	bool m_lastKeyboardKeysPressed[static_cast<int>(KeyboardKey::LAST) + 1];
+	bool m_mouseButtonPressed[static_cast<int>(MouseButton::LAST) + 1];
+	bool m_lastMouseButtonPressed[static_cast<int>(MouseButton::LAST) + 1];
+};
