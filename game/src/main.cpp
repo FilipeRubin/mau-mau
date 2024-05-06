ad783db6ac10995bf43739123bcc69a7fe1c0607
Rubin <filiperubin@gmail.com>
2024-05-05 23:03:59 -0300

    Change testing conditions in game project

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -11,13 +11,26 @@ int main()
 		gameWindow->GetRenderer()->SetBackgroundColor(0.5f, 0.3f, 0.1f, 1.0f);
 		while (gameWindow->ShouldContinueRunning())
 		{
-			if (gameWindow->GetInput()->IsKeyboardKeyJustPressed(KeyboardKey::A))
+			IInput& input = *gameWindow->GetInput();
+			if (input.IsKeyboardKeyJustPressed(KeyboardKey::A))
 			{
 				gameWindow->GetRenderer()->SetBackgroundColor(0.1f, 0.8f, 0.4f, 1.0f);
 			}
-			else if (gameWindow->GetInput()->IsKeyboardKeyJustPressed(KeyboardKey::D))
+			if (input.IsKeyboardKeyJustPressed(KeyboardKey::D))
 			{
 				gameWindow->GetRenderer()->SetBackgroundColor(0.4f, 0.5f, 1.0f, 1.0f);
+			}
+			if (input.IsMouseButtonJustPressed(MouseButton::LEFT))
+			{
+				gameWindow->GetRenderer()->SetBackgroundColor(0.5f, 0.7f, 0.9f, 1.0f);
+			}
+			if (input.IsKeyboardKeyJustReleased(KeyboardKey::SPACE))
+			{
+				gameWindow->GetRenderer()->SetBackgroundColor(0.5f, 0.3f, 0.1f, 1.0f);
+			}
+			if (input.IsMouseButtonPressed(MouseButton::BUTTON_3))
+			{
+				gameWindow->GetRenderer()->SetBackgroundColor(0.5f, 0.5f, 0.6f, 1.0f);
 			}
 			gameWindow->GetRenderer()->ClearScreen();
 			gameWindow->Process();
