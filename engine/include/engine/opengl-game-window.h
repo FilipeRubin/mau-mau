41df55b0c9bcecc909fc919e5a92fcf9bb45e5a3
Rubin <filiperubin@gmail.com>
2024-05-05 21:20:07 -0300

    Implement input handling

--- engine/include/engine/opengl-game-window.h
+++ engine/include/engine/opengl-game-window.h
@@ -4,14 +4,18 @@
 class OpenGLGameWindow : public IGameWindow
 {
 public:
+	static OpenGLGameWindow* GetCurrentInstance();
 	OpenGLGameWindow();
 	~OpenGLGameWindow();
 	void Destroy() override;
+	IInput* GetInput() const override;
 	IRenderer* GetRenderer() const override;
 	void Process() override;
 	bool ShouldContinueRunning() const override;
 	bool TryCreateWindow(int width, int height, const std::string& title) override;
 private:
+	static OpenGLGameWindow* s_currentInstance;
+	IInput* m_input;
 	IRenderer* m_renderer;
 	struct GLFWwindow* m_window;
 };
--- engine/src/opengl-game-window.cpp
+++ engine/src/opengl-game-window.cpp
@@ -1,10 +1,32 @@
 #include "engine/opengl-game-window.h"
+#include "i-input-core.h"
+#include "input-implementation.h"
 #include "opengl-renderer.h"
 #include <GLFW/glfw3.h>
 
+OpenGLGameWindow* OpenGLGameWindow::s_currentInstance = nullptr;
+
+static void glfw_key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	IInputCore& input = *dynamic_cast<IInputCore*>(OpenGLGameWindow::GetCurrentInstance()->GetInput());
+	input.UpdateKeyboardKeyState(static_cast<KeyboardKey>(key), action == GLFW_PRESS);
+}
+
+static void glfw_mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+	IInputCore& input = *dynamic_cast<IInputCore*>(OpenGLGameWindow::GetCurrentInstance()->GetInput());
+	input.UpdateMouseButtonState(static_cast<MouseButton>(button), action == GLFW_PRESS);
+}
+
+OpenGLGameWindow* OpenGLGameWindow::GetCurrentInstance()
+{
+	return s_currentInstance;
+}
+
 OpenGLGameWindow::OpenGLGameWindow() :
-	m_window(nullptr),
-	m_renderer(nullptr)
+	m_input(nullptr),
+	m_renderer(nullptr),
+	m_window(nullptr)
 {
 }
 
@@ -15,11 +37,18 @@ OpenGLGameWindow::~OpenGLGameWindow()
 
 void OpenGLGameWindow::Destroy()
 {
+	if (s_currentInstance == this)
+		s_currentInstance = nullptr;
 	delete m_renderer;
 	m_renderer = nullptr;
 	glfwDestroyWindow(m_window);
 	m_window = nullptr;
 	glfwTerminate();
+}
+
+IInput* OpenGLGameWindow::GetInput() const
+{
+	return m_input;
 }
 
 IRenderer* OpenGLGameWindow::GetRenderer() const
@@ -50,6 +79,10 @@ bool OpenGLGameWindow::TryCreateWindow(int width, int height, const std::string&
 	}
 	glfwMakeContextCurrent(m_window);
 
+	m_input = new InputImplementation();
+
+	glfwSetKeyCallback(m_window, glfw_key_callback);
+
 	m_renderer = new OpenGLRenderer();
 
 	const bool rendererInitializedSuccessfully = dynamic_cast<IRendererCore*>(m_renderer)->TryInitialize();
@@ -59,6 +92,8 @@ bool OpenGLGameWindow::TryCreateWindow(int width, int height, const std::string&
 		Destroy();
 		return false;
 	}
+
+	s_currentInstance = this;
 
 	return true;
 }
