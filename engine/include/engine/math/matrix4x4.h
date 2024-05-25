aed873192e1e3342a96d5632121639951fb9faef
Rubin <filiperubin@gmail.com>
2024-05-25 14:41:24 -0300

    Add operator to sum a Matrix4x4 with a Vector4

--- engine/include/engine/math/matrix4x4.h
+++ engine/include/engine/math/matrix4x4.h
@@ -6,4 +6,5 @@ struct Matrix4x4
 	static Matrix4x4 Perspective(float hFOV, float aspectRatio, float zNear, float zFar);
 	Vector4 value[4];
 	Vector4& operator[](unsigned long long index);
+	Vector4 operator+(const Vector4& other) const;
 };
--- engine/src/engine/math/matrix4x4.cpp
+++ engine/src/engine/math/matrix4x4.cpp
@@ -22,3 +22,17 @@ Vector4& Matrix4x4::operator[](unsigned long long index)
 {
     return value[index];
 }
+
+Vector4 Matrix4x4::operator+(const Vector4& other) const
+{
+    const Vector4& o = other;
+    const Vector4(&v)[4] = value;
+
+    return Vector4
+    (
+        (o[0] * v[0][0]) + (o[1] * v[0][1]) + (o[2] * v[0][2]) + (o[3] * v[0][3]),
+        (o[0] * v[1][0]) + (o[1] * v[1][1]) + (o[2] * v[1][2]) + (o[3] * v[1][3]),
+        (o[0] * v[2][0]) + (o[1] * v[2][1]) + (o[2] * v[2][2]) + (o[3] * v[2][3]),
+        (o[0] * v[3][0]) + (o[1] * v[3][1]) + (o[2] * v[3][2]) + (o[3] * v[3][3])
+    );
+}
