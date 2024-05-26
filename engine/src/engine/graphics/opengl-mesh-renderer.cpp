35276b3f99e695cb224e21a12e6cf6ae205b91fb
Rubin <filiperubin@gmail.com>
2024-05-25 22:50:27 -0300

    Inherit from and implement IMeshRendererCore interface

--- engine/src/engine/graphics/opengl-mesh-renderer.cpp
+++ engine/src/engine/graphics/opengl-mesh-renderer.cpp
@@ -4,3 +4,11 @@ std::shared_ptr<IMesh> OpenGLMeshRenderer::CreateMesh()
 {
     return m_meshes.emplace_back(std::make_shared<OpenGLMesh>());
 }
+
+void OpenGLMeshRenderer::DrawAllMeshes() const
+{
+    for (const std::shared_ptr<OpenGLMesh>& mesh : m_meshes)
+    {
+        mesh->Draw();
+    }
+}
--- engine/src/engine/graphics/opengl-mesh-renderer.h
+++ engine/src/engine/graphics/opengl-mesh-renderer.h
@@ -1,11 +1,13 @@
 #pragma once
 #include "engine/graphics/i-mesh-renderer.h"
+#include "i-mesh-renderer-core.h"
 #include "opengl-mesh.h"
 
-class OpenGLMeshRenderer : public IMeshRenderer
+class OpenGLMeshRenderer : public IMeshRenderer, public IMeshRendererCore
 {
 public:
 	std::shared_ptr<IMesh> CreateMesh() override;
+	void DrawAllMeshes() const override;
 private:
 	std::vector<std::shared_ptr<OpenGLMesh>> m_meshes;
 };
