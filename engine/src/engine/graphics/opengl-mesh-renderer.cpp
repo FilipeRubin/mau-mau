6d24bb0fe454842e9a370f81e577f2abeb7d197d
Rubin <filiperubin@gmail.com>
2024-05-27 11:11:41 -0300

    Add simple hardcoded shader code

--- engine/src/engine/graphics/opengl-mesh-renderer.cpp
+++ engine/src/engine/graphics/opengl-mesh-renderer.cpp
@@ -7,9 +7,30 @@ OpenGLMeshRenderer::OpenGLMeshRenderer()
     OpenGLShader fragmentShader = OpenGLShader(OpenGLShaderType::FRAGMENT_SHADER);
 
     const char* vertSource = R"(
+#version 330 core
+
+layout(location=0) in vec3 v_in_pos;
+layout(location=1) in vec2 v_in_tex;
+layout(location=2) in vec3 v_in_nor;
+
+uniform mat4 u_projection;
+uniform mat4 u_model;
+
+void main()
+{
+    gl_Position = u_projection * u_model * vec4(v_in_pos, 1.0);
+}
 )";
     
     const char* fragSource = R"(
+#version 330 core
+
+out vec4 f_fragColor;
+
+void main()
+{
+    f_fragColor = vec4(1.0);
+}
 )";
 
     vertexShader.SetSource(vertSource);
