ff47109310d9e8e76a9f899cb5081edf3b7aeab1
Rubin <filiperubin@gmail.com>
2024-05-31 00:59:26 -0300

    Add default values for Transform3D members

--- engine/include/engine/math/transform3d.h
+++ engine/include/engine/math/transform3d.h
@@ -4,7 +4,7 @@
 class Transform3D
 {
 public:
-	Vector3 position;
-	Vector3 rotation;
-	Vector3 scale;
+	Vector3 position = Vector3(0.0f, 0.0f, 0.0f);
+	Vector3 rotation = Vector3(0.0f, 0.0f, 0.0f);
+	Vector3 scale = Vector3(1.0f, 1.0f, 1.0f);
 };
