abf6e3e8223ae0c167e346ea02444ffda70ffbf2
Rubin <filiperubin@gmail.com>
2024-06-25 19:53:04 -0300

    Modify methods to return success or failure

--- engine/include/engine/io/i-source-io.h
+++ engine/include/engine/io/i-source-io.h
@@ -5,6 +5,6 @@ class ISourceIO
 {
 public:
 	virtual ~ISourceIO() = default;
-	virtual std::string Read() = 0;
-	virtual void Write(const std::string& content) = 0;
+	virtual bool TryRead(std::string& result) = 0;
+	virtual bool TryWrite(const std::string& content) = 0;
 };
